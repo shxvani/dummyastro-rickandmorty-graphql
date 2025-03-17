128e6739fd35c1f97cb56ebdc558ecc6f951e848
Shivani S <SESA755125@se.com>
2025-03-17 11:52:58 +0530

    w

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+// https://astro.build/config
+export default defineConfig({
+  // your existing configuration
+});
